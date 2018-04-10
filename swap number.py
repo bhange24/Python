f45123eea90d32414c4180fb517584b062099e85
bhange24 <38123878+bhange24@users.noreply.github.com>
2018-04-10 09:58:55 +0530

    Add files via upload

---
+++ swap number.py
@@ -0,0 +1,15 @@
+# Python program to swap two variables
+
+# To take input from the user
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
+
+# x = 5
+# y = 10
+
+swap = x
+x = y
+y = swap
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
