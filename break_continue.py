ed051effc2cf9d9d5a7138d955b38ca488a01cb1
bhange24 <shraddha.bhange24@gmail.com>
2018-04-13 17:49:15 +0530

    break and continue

---
+++ break_continue.py
@@ -0,0 +1,11 @@
+for value in "SHRADDHA":
+    if value == "D":
+        break
+    print(value)
+print("End of Loop")
+
+for val in "SHRADDHA":
+    if val == "D":
+        continue
+    print(val)
+print("End of Loop")
